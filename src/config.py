3d628ceacf820fd5c4cdcb63d95c5a92cd8c6427
RomesAll <romesky845@gmail.com>
2026-05-23 15:00:43 +0300

    refactor(config): обновить вывод из env файла
    - Класс PostgresSettings стал обычной pydantic моделью
    - Для класса Settings с помощью model_config было настроено поведения при котором вложенные модели (атрибут postgres и тд) заполнялись данными с помощью разделителя (аргумент env_nested_delimiter) из env файла

--- src/config.py
+++ src/config.py
@@ -1,13 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel
 from src import BASE_DIR
 
-class PostgresSettings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=f'{BASE_DIR}/.env.postgresql',
-        env_prefix='POSTGRES_',
-        env_file_encoding='utf-8',
-        extra='ignore'
-    )
+class PostgresSettings(BaseModel):
     HOST: str
     PORT: int
     USER: str
@@ -19,6 +14,17 @@ class PostgresSettings(BaseSettings):
         return f'postgresql+psycopg2://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DB}'
 
 class Settings(BaseSettings):
-    postgres: PostgresSettings = PostgresSettings()
+    postgres: PostgresSettings
+
+    app_name: str = "ResumesManagerApp"
+    debug: bool = False
+
+    model_config = SettingsConfigDict(
+        env_prefix='APP_',
+        env_file=f'{BASE_DIR}/.env.postgresql',
+        env_file_encoding='utf-8',
+        env_nested_delimiter='__'
+    )
 
 settings = Settings()
+print(settings)
